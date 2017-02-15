2d5b1dc92d299cb4ff9ac9f9a14f22b06028c967
Mark Gaiser <markg85@gmail.com>
2017-02-15 23:03:45 +0100

    Modify the 'latest:' functionality to use Promise as well.

--- latest.js
+++ latest.js
@@ -22,35 +22,38 @@
 let curl = require('curlrequest');
 
 module.exports = {
-  returnData: [],
-
   zeroPad: function(num, places) {
     var zero = places - num.toString().length + 1;
     return Array(+(zero > 0 && zero)).join("0") + num;
   },
 
-  fetch: function(searchString, callback){
-    let options = {
-      url: `http://api.tvmaze.com/singlesearch/shows?q=${encodeURIComponent(searchString)}&embed=previousepisode`,
-      useragent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
-      insecure: true,
-      timeout: 5,
-      retries: 3
-    };
+  execute: function() {
+    return new Promise((resolve, reject) => {
+      let searchString = module.exports.searchString
+      let options = {
+        url: `http://api.tvmaze.com/singlesearch/shows?q=${encodeURIComponent(searchString)}&embed=previousepisode`,
+        useragent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        insecure: true,
+        timeout: 5,
+        retries: 3
+      };
 
-    curl.request(options, function (err, data, meta) {
-        module.exports.returnData = {error: "No valid episode information found in tvmaze. We used url: " + options.url}
+      curl.request(options, function (err, data, meta) {
 
         if (data) {
-            let obj = JSON.parse(data)
-
-            if (obj && obj._embedded && obj._embedded.previousepisode) {
-                let previousEpisodeData = obj._embedded.previousepisode;
-                module.exports.returnData = {episodeSuffix: `S${module.exports.zeroPad(previousEpisodeData.season, 2)}E${module.exports.zeroPad(previousEpisodeData.number, 2)}`, episodeName: previousEpisodeData.name, rawData: obj}
-            }
+          let obj = JSON.parse(data)
+          if (obj && obj._embedded && obj._embedded.previousepisode) {
+            let previousEpisodeData = obj._embedded.previousepisode;
+            resolve({episodeSuffix: `S${module.exports.zeroPad(previousEpisodeData.season, 2)}E${module.exports.zeroPad(previousEpisodeData.number, 2)}`, episodeName: previousEpisodeData.name, rawData: obj})
+          }
+        } else {
+          reject(err)
         }
 
-        callback()
-    });
-  }
+        resolve({error: "No valid episode information found in tvmaze. We used url: " + options.url})
+      });
+
+    })
+  },
+  searchString: ""
 };
