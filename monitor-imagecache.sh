5fa98360dd277fc06d87b7c590bec225c5f987a2
Mark Gaiser <markg85@gmail.com>
2019-12-15 23:54:59 +0100

    Create a WebP image from the input. This would alow a later migration from jpg to webp.

--- monitor-imagecache.sh
+++ monitor-imagecache.sh
@@ -3,5 +3,12 @@
 DIR=/home/docker-data/tor/imagecache
 inotifywait -e CLOSE_WRITE -m -r $DIR --format %f |
 while read RES
-   do ln $DIR/$RES /home/docker-data/static_sites/i/$RES; done
+   do
+      filename="${RES%%.*}"
+      extension="${RES#*.}"
+      if [ "$extension" != "webp" ]; then
+         cwebp -q 85 $DIR/$RES -o $DIR/$filename.webp -quiet
+      fi
+      ln $DIR/$RES /home/docker-data/static_sites/i/$RES;
+   done
 
